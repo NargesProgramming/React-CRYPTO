71ff85d3c4c132e4a1b7cce7e7024bedd241ee51
NargesProgramming <nargesnahriprogramming@gmail.com>
2022-05-01 10:29:44 +0430

    Crypto Project Committed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+//components
+import Landing from "./components/Landing"
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+};
+
+export default App;
